f0ecb7c37e479f921f5eeecc76f4544d03c9e847
mehtapradnyatama <mehtapradnyatama@gmail.com>
2025-10-21 19:50:51 +0700

    Update exercise.jsx

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -10,7 +10,7 @@ function Exercise() {
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
           <UserCard name="Joel Miller" email="joel@example.com" street="123 Main St" city="Anytown" />
-          <UserCard name="Johnny Sins" email="johnny@example.com" street="456 Elm St" city="Othertown" />
+          <UserCard name="Talon" email="talon@example.com" street="456 Elm St" city="Othertown" />
           <UserCard name="Dive iPhone" email="dive@example.com" street="789 Oak St" city="Sometown" />
         </div>
       </div>
