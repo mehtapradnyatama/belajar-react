ba5b643d4ff583a83e7386d4e18e1e70d6c59700
mehtapradnyatama <mehtapradnyatama@gmail.com>
2025-10-21 20:25:40 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import Exercise from './Exercise.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <Exercise />
-  </React.StrictMode>,
+  </StrictMode>,
 )
