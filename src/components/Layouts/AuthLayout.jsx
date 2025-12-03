4664b9c2eb135e061fca50a1901425afc7736350
mehtapradnyatama <mehtapradnyatama@gmail.com>
2025-12-03 21:31:16 +0700

    atomic design page sign in dan sign up

---
+++ src/components/Layouts/AuthLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../Elements/Logo";
+
+function AuthLayout(props) {
+  const { children } = props;
+  return (
+    <>
+      <main className="min-h-screen bg-special-mainBg flex justify-center items-center">
+        {/* container start */}
+        <div className="w-full max-w-sm">
+          <Logo />
+          {children}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default AuthLayout;
--- src/latihan/Exercise.jsx
+++ src/latihan/Exercise.jsx
@@ -2,7 +2,7 @@ import React, { use, useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import { getUsers } from "./Services";
 import PostCard from "./PostCard";
-import postsData from "../postsData";
+import postsData from "../../postsData";
 
 function Exercise() {
   const [users, setUsers] = useState([]);
